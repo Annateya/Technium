d3caa5dc83d16eb8d216a0ab5cf973955890afd7
Мария <m.nedbailo@yandex.ru>
2025-08-08 01:34:43 +0300

    Обход графа в ширину BFS

---
+++ Technium/Module_4/4.15_BFS.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({self.value})'
+
+class Graph:
+    def __init__(self, root):
+        self._root = root
+
+    def dfs(self, vertex, visited):
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.append(vertex)
+            for neighbor in vertex.outbound:
+                self.dfs(neighbor, visited)
+        return visited
+
+    def bfs(self, start):
+        visited = set()  # Множество для отслеживания посещенных вершин
+        queue = deque()  # Очередь для обхода
+
+        queue.append(start)
+        visited.add(start)
+
+        while queue:
+            vertex = queue.popleft()
+            print(vertex)  # Посещаем вершину
+
+            for neighbor in vertex.outbound:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+        return visited
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+i = Node('i')
+k = Node('k')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+a.point_to(e)
+e.point_to(f)
+e.point_to(g)
+f.point_to(i)
+f.point_to(h)
+g.point_to(k)
+
+g = Graph(a)
+
+visited = []
+g.dfs(a, visited)
+g.bfs(a)
